845fafd70e07bd578ba83845b28d138748596612
Holger Neuner <neuner@47gradnord.de>
2020-07-03 17:00:38 +0200

    initial commit

---
+++ src/ContaoCleverreachConnectorBundle.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * bundle.cleverreach-connect for Contao Open Source CMS
+ *
+ * Copyright (C) 2020 47GradNord - Agentur für Internetlösungen
+ *
+ * @license    commercial
+ * @author     Holger Neuner
+ */
+
+
+namespace Webandwork\ContaoCleverreachConnectorBundle;
+
+
+class ContaoCleverreachConnectorBundle
+{
+
+}
